b23eccca1f860a9da37d711aa6e8e723d74ba8a1
masum <masummir.lm@gmail.com>
2022-12-25 17:17:23 +0600

    project update

---
+++ SpringJDBC/src/main/java/com/test/AutoWiringCofig.java
@@ -0,0 +1,35 @@
+package com.test;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"com.test.dao"})
+public class AutoWiringCofig {
+	
+	@Bean("ds")
+	public DataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/test");
+		ds.setUsername("root");
+		ds.setPassword("root");
+		
+		return ds;
+	}
+	
+	@Bean("jdbcTemplate")
+	public JdbcTemplate getJdbcTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(getDataSource());
+		
+		return jdbcTemplate;
+	}
+	
+
+}
