e85ecc80049c5af314ac68f1043191cde3d99eb4
masum <pridesys.8418@gmail.com>
2022-12-29 20:38:28 +0600

    update

--- JavaCoreConcept/src/main/java/com/program/javatpoint/FrequencyEachElementArray.java
+++ JavaCoreConcept/src/main/java/com/program/javatpoint/FrequencyEachElementArray.java
@@ -3,15 +3,14 @@ package com.program.javatpoint;
 public class FrequencyEachElementArray {
 	public static void main(String[] args) {
 		
-		int arr1[] = new int[] {1,2,3,4,5,6,23,454,2,3,4,1,1,1,1};
+		int arr1[] = {1,2,3,4,5,1,2,3,2,6 };
 		int arr2[] = new int[arr1.length];
-		
 		
 		int visited = -1;
 		
 		for(int i=0; i<arr1.length; i++) {
 			int count = 1;
-			for(int j=0; j<arr1.length; j++) {
+			for(int j=i+1; j<arr1.length; j++) {
 				if(arr1[i] == arr1[j]) {
 					count ++;
 					
@@ -23,9 +22,9 @@ public class FrequencyEachElementArray {
 			}
 		}
 		
-		for(int i=0; i<arr2.length;i++) {
+		for(int i=0; i<arr1.length;i++) {
 			if(arr2[i] != visited) {
-				System.out.print(arr1[i] + " || " + arr2[i]);
+				System.out.println(arr1[i] + " || " + arr2[i]);
 			}
 		}
 		
